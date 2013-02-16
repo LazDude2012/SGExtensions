55d162854881a648289dfd936bcfdcfee973607e
LazDude2012 <misunderstoodawesomeness@gmail.com>
2013-02-15 23:01:39 -0700

    Adding config

--- ConfigHandler.java
+++ ConfigHandler.java
@@ -1,6 +1,20 @@
 package sgextensions;
 
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.common.Configuration;
+import net.minecraftforge.common.Property;
+
 public class ConfigHandler
 {
+	public static int blockDiallerID;
 
+	public static void loadConfig(FMLPreInitializationEvent e){
+		Configuration config = new Configuration(e.getSuggestedConfigurationFile());
+		config.load();
+
+		Property dialler;
+		dialler = config.getBlock("DialComputerID",3745,"The Block ID for the Dialling Computer.");
+
+		config.save();
+	}
 }
--- SGExtensions.java
+++ SGExtensions.java
@@ -3,6 +3,7 @@ package sgextensions;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
@@ -15,11 +16,16 @@ public class SGExtensions
 	@SidedProxy(clientSide="sgextensions.ClientProxy",serverSide = "sgextensions.CommonProxy")
 	public static CommonProxy proxy;
 	public static Block diallerBlock;
+
+	@Mod.PreInit()
+	public void preInit(FMLPreInitializationEvent e){
+		ConfigHandler.loadConfig(e);
+		diallerBlock = new DiallerBlock(ConfigHandler.blockDiallerID,0).setBlockName("diallerblock");
+	}
 	@Mod.Init
 	public void load(FMLInitializationEvent event)
 	{
 		proxy.registerRenderThings();
-		diallerBlock=new DiallerBlock(4242,0).setBlockName("diallerBlock");
 		GameRegistry.registerBlock(diallerBlock, "Dialler");
 		LanguageRegistry.addName(diallerBlock,"Gate Dialing Device");
 	}
